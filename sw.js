0e91216e1bbbb9bfcf35fc8b52e4488b70d91438
liquidliang <liquidliang@tencet.com>
2017-02-13 17:17:44 +0800

    [update] article

--- sw.js
+++ sw.js
@@ -160,7 +160,7 @@ self.addEventListener('fetch', function (event) {
   }
 
   if(/\.jpg$|\.png$|\.gif$/.test(url)){
-    return;
+    return; //这些资源如果不是https协议 下面那句会报错
   }
 
   return event.respondWith(fetch(req));
