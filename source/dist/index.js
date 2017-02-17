0ba3a802ac7dfd9fe28ed1a95f3acd0e1511b044
liquidliang <liquidliang@tencet.com>
2017-02-17 17:37:54 +0800

    [update] article

--- source/dist/index.js
+++ source/dist/index.js
@@ -1623,16 +1623,21 @@
 	
 	            slidebar.content = content.replace(/<%(([^>]|[^%]>)+)%>/g, function ($0, $1) {
 	              var item = {};
-	              if (/^\[[^)]+\)$/.test($1)) {
+	              var fileName = '';
+	              if ($1.indexOf(']') > 0) {
 	                //这种格式：[描述](相对与当前目录的地址)
 	                var arr = $1.substr(1, $1.length - 2).split(/\]\s*\(/);
 	                item.title = arr[0] || '';
 	                item.href = baseHash + '/' + (arr[1] || '');
+	                fileName = arr[1];
 	              } else {
 	                item.title = $1;
 	                item.href = baseHash + '/' + $1 + '.md';
+	                fileName = $1 + '.md';
 	              }
-	              chapters.push(item);
+	              if (m_article.hasArticle(key + '/' + fileName)) {
+	                chapters.push(item);
+	              }
 	              return '<a data-on="?m=replaceHash" data-url="' + item.href + '">' + item.title + '</a>';
 	            });
 	            viewSlidebar.reset(slidebar);
@@ -1641,13 +1646,15 @@
 	            });
 	          })();
 	        }
-	        var fileName = location.hash.replace(baseHash, '');
-	        if (fileName) {
-	          m_article.getArticleContent(key + fileName).then(function (data) {
+	        var fileName = key + location.hash.replace(baseHash, '');
+	        if (m_article.hasArticle(fileName)) {
+	          m_article.getArticleContent(fileName).then(function (data) {
 	            viewContent.reset(data);
 	          });
-	        } else {
+	        } else if (slidebar.chapters[0]) {
 	          return BCD.replaceHash(slidebar.chapters[0].href);
+	        } else {
+	          viewContent.reset({});
 	        }
 	      });
 	    }
--- source/src/page/book.js
+++ source/src/page/book.js
@@ -4,7 +4,7 @@ const c_articleList = require('card/blog/article_list');
 
 
 
-module.exports = function(page, key) {
+module.exports = function (page, key) {
   page.html(s_mainContainer);
   let viewContent = page.find('[data-selector="main"]');
   let viewSlidebar = page.find('[data-selector="slidebar"]');
@@ -23,44 +23,53 @@ module.exports = function(page, key) {
 
   page.setView({
     title: m_article.getName(key),
-    start: function(){
-      if(currentHash!==location.hash){
+    start: function () {
+      if (currentHash !== location.hash) {
         viewContent.empty();
         currentHash = location.hash;
       }
       this.show();
-      m_article.getArticleContent(m_article.getSidebarPath(key)).then((data)=>{
-        let baseHash = '#!/'+BCD.getHash(0);
-        if(!slidebar){
+      m_article.getArticleContent(m_article.getSidebarPath(key)).then((data) => {
+        let baseHash = '#!/' + BCD.getHash(0);
+        if (!slidebar) {
           slidebar = $.extend({}, data);
           let content = slidebar.content || '';
           let chapters = slidebar.chapters = [];
 
-          slidebar.content = content.replace(/<%(([^>]|[^%]>)+)%>/g,function($0, $1){
+          slidebar.content = content.replace(/<%(([^>]|[^%]>)+)%>/g, function ($0, $1) {
             let item = {};
-            if(/^\[[^)]+\)$/.test($1)){//这种格式：[描述](相对与当前目录的地址)
-              let arr = $1.substr(1, $1.length-2).split(/\]\s*\(/);
+            let fileName = '';
+            if ($1.indexOf(']')>0) { //这种格式：[描述](相对与当前目录的地址)
+              let arr = $1.substr(1, $1.length - 2).split(/\]\s*\(/);
               item.title = arr[0] || '';
-              item.href = baseHash + '/' + (arr[1] || '');
-            }else{
+              item.href  = baseHash + '/' + (arr[1] || '');
+              fileName = arr[1];
+            } else {
               item.title = $1;
-              item.href = baseHash + '/'+$1+'.md';
+              item.href = baseHash + '/' + $1 + '.md';
+              fileName = $1 + '.md';
             }
-            chapters.push(item);
-            return '<a data-on="?m=replaceHash" data-url="'+item.href+'">'+item.title+'</a>';
+            if(m_article.hasArticle(key+'/'+fileName)){
+              chapters.push(item);
+            }
+            return '<a data-on="?m=replaceHash" data-url="' + item.href + '">' + item.title + '</a>';
           });
           viewSlidebar.reset(slidebar);
-          setTimeout(function(){
+          setTimeout(function () {
             viewSlidebar.bindEvent();
           });
         }
-        let fileName = location.hash.replace(baseHash, '');
-        if(fileName){
-          m_article.getArticleContent(key + fileName).then((data)=>{
+        let fileName = key + location.hash.replace(baseHash, '');
+        if (m_article.hasArticle(fileName)) {
+          m_article.getArticleContent(fileName).then((data) => {
             viewContent.reset(data);
           });
-        }else{
+        } else if (slidebar.chapters[0]) {
           return BCD.replaceHash(slidebar.chapters[0].href);
+        } else {
+          viewContent.reset({
+
+          });
         }
       });
     }
