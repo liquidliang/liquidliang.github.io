1d53f8e29a832494f000a94f80fcdfdab3af2056
liquidliang <ljquan@qq.com>
2017-05-14 23:21:54 +0800

    [update] article

--- source/src/model/read_history.js
+++ source/src/model/read_history.js
@@ -17,6 +17,12 @@ const addHistory = (path)=>{
   localStorage.setItem(storageKey, JSON.stringify(readHistory));
 };
 
+BCD.addEvent('article_down', function(ele){
+  ele.on('click', function(e){
+    ele.hide();
+    addHistory(ele.data('url'));
+  });
+});
 
 module.exports = {
   addHistory,
