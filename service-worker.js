de8f40783ba71f7846c3a1dda5f8cc68d51af46f
liquidliang <ljquan@qq.com>
2017-05-15 22:54:37 +0800

    [update] article

--- service-worker.js
+++ service-worker.js
@@ -103,77 +103,76 @@ function _fetch(url, timeout) {
 }
 //更新缓存
 var addToCache = function (dbName, req, response) {
-  return _fetch(req.clone());
-  // return _fetch(req.clone()).then(function (resp) {
-  //   if (resp.type !== 'basic' && resp.type !== 'cors') {
-  //     return resp;
-  //   }
-  //
-  //   if (resp.status !== 200) {
-  //     throw new Error('response status is ' + resp.status);
-  //   }
-  //   var contentType = resp.headers.get('content-type');
-  //   if (contentType.indexOf('text/html') > -1) {
-  //     //避免缓存了上网时需要先登录的页面
-  //     var arr = req.url.match(/\.[a-z]+$/);
-  //     if (arr) {
-  //       var ext = arr[0].substr(1);
-  //       if (ext != 'html') {
-  //         throw new Error('response content-type is ' + contentType);
-  //       }
-  //     }
-  //   }
-  //
-  //   var cacheResp = resp.clone();
-  //   if (dbName === imageCacheName && !/^image\//.test(contentType)) {
-  //     return resp;
-  //   }
-  //   caches.open(dbName).then(function (cache) {
-  //     //删除旧文件
-  //     cache.keys().then(function (oldReqList) {
-  //       if (req.url.indexOf('?') > 0) {
-  //         let urlKey = getNoSearch(req.url) + '?';
-  //         oldReqList.filter(oldReq => oldReq.url.indexOf(urlKey) > -1).forEach(function (oldReq) {
-  //           cache.delete(oldReq);
-  //         });
-  //       }
-  //       //添加新文件
-  //       cache.put(req.clone(), cacheResp);
-  //     });
-  //   });
-  //
-  //   return resp;
-  // }).catch(function (error) {
-  //   if (response) { //请求失败用缓存保底
-  //     console.log(`[ServiceWorker] fetch failed (${ req.url }) and use cache`, error);
-  //     return response;
-  //   } else {
-  //     return caches.open(dbName).then(function (cache) {
-  //       //取旧缓存
-  //       let urlKey = getNoSearch(req.url);
-  //       return cache.keys().then(function (oldReqList) {
-  //         let oldReq;
-  //         while (oldReq = oldReqList.pop()) {
-  //           if (oldReq.url.indexOf(urlKey) > -1) {
-  //             return cache.match(oldReq)
-  //           }
-  //         }
-  //         return null;
-  //       });
-  //     }).then(function (resp) {
-  //       if (resp) {
-  //         console.log(`[ServiceWorker] fetch failed (${ req.url }) and use old cache`, error);
-  //         return resp;
-  //       }
-  //       // Respond with a 400 "Bad Request" status.
-  //       console.log(`[ServiceWorker] fetch failed: ${ req.url }`, error);
-  //       return new Response(new Blob, {
-  //         'status': 400,
-  //         'statusText': 'Bad Request'
-  //       });
-  //     });
-  //   }
-  // });
+  return _fetch(req.clone()).then(function (resp) {
+    if (resp.type !== 'basic' && resp.type !== 'cors') {
+      return resp;
+    }
+
+    if (resp.status !== 200) {
+      throw new Error('response status is ' + resp.status);
+    }
+    var contentType = resp.headers.get('content-type');
+    if (contentType.indexOf('text/html') > -1) {
+      //避免缓存了上网时需要先登录的页面
+      var arr = req.url.match(/\.[a-z]+$/);
+      if (arr) {
+        var ext = arr[0].substr(1);
+        if (ext != 'html') {
+          throw new Error('response content-type is ' + contentType);
+        }
+      }
+    }
+
+    var cacheResp = resp.clone();
+    if (dbName === imageCacheName && !/^image\//.test(contentType)) {
+      return resp;
+    }
+    // caches.open(dbName).then(function (cache) {
+    //   //删除旧文件
+    //   cache.keys().then(function (oldReqList) {
+    //     if (req.url.indexOf('?') > 0) {
+    //       let urlKey = getNoSearch(req.url) + '?';
+    //       oldReqList.filter(oldReq => oldReq.url.indexOf(urlKey) > -1).forEach(function (oldReq) {
+    //         cache.delete(oldReq);
+    //       });
+    //     }
+    //     //添加新文件
+    //     cache.put(req.clone(), cacheResp);
+    //   });
+    // });
+
+    return resp;
+  }).catch(function (error) {
+    if (response) { //请求失败用缓存保底
+      console.log(`[ServiceWorker] fetch failed (${ req.url }) and use cache`, error);
+      return response;
+    } else {
+      return caches.open(dbName).then(function (cache) {
+        //取旧缓存
+        let urlKey = getNoSearch(req.url);
+        return cache.keys().then(function (oldReqList) {
+          let oldReq;
+          while (oldReq = oldReqList.pop()) {
+            if (oldReq.url.indexOf(urlKey) > -1) {
+              return cache.match(oldReq)
+            }
+          }
+          return null;
+        });
+      }).then(function (resp) {
+        if (resp) {
+          console.log(`[ServiceWorker] fetch failed (${ req.url }) and use old cache`, error);
+          return resp;
+        }
+        // Respond with a 400 "Bad Request" status.
+        console.log(`[ServiceWorker] fetch failed: ${ req.url }`, error);
+        return new Response(new Blob, {
+          'status': 400,
+          'statusText': 'Bad Request'
+        });
+      });
+    }
+  });
 };
 
 
