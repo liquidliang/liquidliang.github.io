9217f7ca3e4bd95aba4efdcacddc078cdc4c668b
liquidliang <liquidliang@tencet.com>
2017-05-15 21:19:45 +0800

    [update] article

--- service-worker.js
+++ service-worker.js
@@ -73,17 +73,158 @@ self.addEventListener('activate', function (event) {
     }));
 });
 
-//Service Worker 请求拦截事件
-this.addEventListener('fetch', function(event)  {
-  event.respondWith(
-    caches.open(businessKey).then(function(cache) {
-      return cache.match(event.request.url);
-    }).then(function(response){
-      //response为空表明未匹配成功，交由fetch方法去网络拉取
-      if(response) {
-        return response;
+
+var getNoSearch = function (url) {
+  return url.replace(/\?[^?]+/, '');
+}
+
+function _fetch(url, timeout) {
+  var abort_fn = null;
+  var fetch_promise = fetch(url);
+
+  //这是一个可以被reject的promise
+  var abort_promise = new Promise(function (resolve, reject) {
+    abort_fn = function () {
+      reject(new Error('fetch timeout!'));
+    };
+  });
+
+  //这里使用Promise.race，以最快 resolve 或 reject 的结果来传入后续绑定的回调
+  var abortable_promise = Promise.race([
+    fetch_promise,
+    abort_promise
+  ]);
+
+  setTimeout(function () {
+    abort_fn();
+  }, timeout || 2E3);
+
+  return abortable_promise;
+}
+//更新缓存
+var addToCache = function (dbName, req, response) {
+
+  return _fetch(req.clone()).then(function (resp) {
+    if (resp.type !== 'basic' && resp.type !== 'cors') {
+      return resp;
+    }
+
+    if (resp.status !== 200) {
+      throw new Error('response status is ' + resp.status);
+    }
+    var contentType = resp.headers.get('content-type');
+    if (contentType.indexOf('text/html') > -1) {
+      //避免缓存了上网时需要先登录的页面
+      var arr = req.url.match(/\.[a-z]+$/);
+      if (arr) {
+        var ext = arr[0].substr(1);
+        if (ext != 'html') {
+          throw new Error('response content-type is ' + contentType);
+        }
       }
-      return fetch(event.request);
-    })
-  );
+    }
+
+    var cacheResp = resp.clone();
+    if (dbName === imageCacheName && !/^image\//.test(contentType)) {
+      return resp;
+    }
+    caches.open(dbName).then(function (cache) {
+      //删除旧文件
+      cache.keys().then(function (oldReqList) {
+        if (req.url.indexOf('?') > 0) {
+          let urlKey = getNoSearch(req.url) + '?';
+          oldReqList.filter(oldReq => oldReq.url.indexOf(urlKey) > -1).forEach(function (oldReq) {
+            cache.delete(oldReq);
+          });
+        }
+        //添加新文件
+        cache.put(req.clone(), cacheResp);
+      });
+    });
+
+    return resp;
+  }).catch(function (error) {
+    if (response) { //请求失败用缓存保底
+      console.log(`[ServiceWorker] fetch failed (${ req.url }) and use cache`, error);
+      return response;
+    } else {
+      return caches.open(dbName).then(function (cache) {
+        //取旧缓存
+        let urlKey = getNoSearch(req.url);
+        return cache.keys().then(function (oldReqList) {
+          let oldReq;
+          while (oldReq = oldReqList.pop()) {
+            if (oldReq.url.indexOf(urlKey) > -1) {
+              return cache.match(oldReq)
+            }
+          }
+          return null;
+        });
+      }).then(function (resp) {
+        if (resp) {
+          console.log(`[ServiceWorker] fetch failed (${ req.url }) and use old cache`, error);
+          return resp;
+        }
+        // Respond with a 400 "Bad Request" status.
+        console.log(`[ServiceWorker] fetch failed: ${ req.url }`, error);
+        return new Response(new Blob, {
+          'status': 400,
+          'statusText': 'Bad Request'
+        });
+      });
+    }
+  });
+};
+
+
+var fetchCache = function (dbName, req) {
+  return caches.open(dbName).then(function (cache) {
+    return cache.match(req.clone());
+  }).then(function (response) {
+    if (response) {
+      if (dbName == businessCacheName) {
+        addToCache(dbName, req, response); //更新缓存，下次使用
+      }
+      return response; //如果命中缓存，直接使用缓存.
+    } else {
+      return addToCache(dbName, req);
+    }
+  }).catch(function (e) {
+    console.log(e);
+    return addToCache(dbName, req);
+  });
+}
+self.addEventListener('fetch', function (event) {
+
+  var req, url = event.request.url;
+  var requestURL = new URL(url);
+
+  // if (url.indexOf('http:') === 0) {
+  //   return event.respondWith(fetch(event.request.clone()));
+  // }
+
+  if (requestURL.search.indexOf('cors=1') !== -1) {
+    req = new Request(url, {
+      mode: 'cors'
+    });
+  } else {
+    req = event.request.clone();
+  }
+
+  if (FILES.indexOf(requestURL.pathname) > -1) {
+    return event.respondWith(fetchCache(businessCacheName, req));
+  }
+
+  for (var dbName in regDict) {
+    if (regDict[dbName].test(url)) {
+      return event.respondWith(fetchCache(dbName, req));
+    }
+  }
+
+  if (requestURL.protocol != 'https:' || /\.json$/.test(url)) {
+    return;
+  }
+
+  return event.respondWith(fetchCache(imageCacheName, req));
+
 });
