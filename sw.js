5fbefd279e31eca436e44e71f7a08449aa5d61e2
liquidliang <liquidliang@tencet.com>
2017-05-16 21:31:45 +0800

    [update] article

--- sw.js
+++ sw.js
@@ -32,17 +32,17 @@ var consoleList = [];
 var matchAll = self.clients.matchAll || self.clients.getAll;
 
 self.addEventListener('install', function (event) {
-  consoleLog('[ServiceWorker] Installed version', version);
+  console.log('[ServiceWorker] Installed version', version);
   event.waitUntil(caches.open(businessCacheName).then(function (cache) {
     return cache.addAll(FILES);
   }).then(function () {
-    consoleLog('[ServiceWorker] Skip waiting on install');
+    console.log('[ServiceWorker] Skip waiting on install');
     return self.skipWaiting();
   }));
 });
 
 self.addEventListener('activate', function (event) {
-  consoleLog('self.clients.matchAll', !!self.clients.matchAll);
+  console.log('self.clients.matchAll', !!self.clients.matchAll);
   matchAll.call(clients, {
     includeUncontrolled: true
   }).then(function (clientList) {
@@ -50,7 +50,7 @@ self.addEventListener('activate', function (event) {
       return client.url;
     });
     //如果新sw生效，对其他页面造成影响，这里可以查
-    consoleLog('[ServiceWorker] Matching clients:', urls.join(', '));
+    console.log('[ServiceWorker] Matching clients:', urls.join(', '));
   });
 
   event.waitUntil(
@@ -59,14 +59,14 @@ self.addEventListener('activate', function (event) {
         cacheNames.map(function (cacheName) {
           // 删除掉当前定义前缀中不在expectedCaches中的缓存集
           if (nameReg.test(cacheName) && expectedCaches.indexOf(cacheName) == -1) {
-            consoleLog('[ServiceWorker] Deleting old cache:', cacheName);
+            console.log('[ServiceWorker] Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     }).then(function () {
       //使service worker立马生效，在单页面应用中，这样是合理的
-      consoleLog('[ServiceWorker] Claiming clients for version', version);
+      console.log('[ServiceWorker] Claiming clients for version', version);
       return self.clients.claim();
     }));
 });
@@ -144,7 +144,7 @@ var addToCache = function (dbName, req, response) {
     return resp;
   }).catch(function (error) {
     if (response) { //请求失败用缓存保底
-      consoleLog('[ServiceWorker] fetch failed ('+req.url+') and use cache', error);
+      console.log('[ServiceWorker] fetch failed ('+req.url+') and use cache', error);
       return response;
     } else {
       return caches.open(dbName).then(function (cache) {
@@ -161,11 +161,11 @@ var addToCache = function (dbName, req, response) {
         });
       }).then(function (resp) {
         if (resp) {
-          consoleLog('[ServiceWorker] fetch failed ('+req.url+') and use old cache', error);
+          console.log('[ServiceWorker] fetch failed ('+req.url+') and use old cache', error);
           return resp;
         }
         // Respond with a 400 "Bad Request" status.
-        consoleLog('[ServiceWorker] fetch failed ('+req.url+')', error);
+        console.log('[ServiceWorker] fetch failed ('+req.url+')', error);
         return new Response(new Blob, {
           'status': 400,
           'statusText': 'Bad Request'
@@ -189,7 +189,7 @@ var fetchCache = function (dbName, req) {
       return addToCache(dbName, req);
     }
   }).catch(function (e) {
-    consoleLog(e);
+    console.log(e);
     return addToCache(dbName, req);
   });
 }
@@ -377,11 +377,11 @@ function _processMessage(msgObj, option) {
       });
     default:
       return new Promise(function (resolve) {
-        resolve(consoleLog('msgObj.m=' + msgObj.m + ' match nothing!'));
+        resolve(console.log('msgObj.m=' + msgObj.m + ' match nothing!'));
       });
     }
   } catch (e) {
-    consoleLog('_processMessage', e.stack);
+    console.log('_processMessage', e.stack);
   }
 }
 
